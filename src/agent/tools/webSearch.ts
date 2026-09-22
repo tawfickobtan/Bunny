550f5212de0c0e8cc6ada538c7f2fd62116ddb75
Tawfic Kobtan <tawfikcraft@gmail.com>
2026-09-22 14:34:11 +0300

    feat: web search tool and schema

--- src/agent/tools/webSearch.ts
+++ src/agent/tools/webSearch.ts
@@ -1,6 +1,30 @@
 import {tavily} from "@tavily/core";
+import type {tool} from '../types/tool.js';
 
-const tvly = tavily({ apiKey: "tvly-YOUR_API_KEY" });
-const response = await tvly.search("Who is Leo Messi?");
+const client = tavily({ apiKey: process.env.TAVILY_API_KEY as string});
 
-console.log(response);
+export async function webSearch({query}: {query: string}): Promise<string>{
+    const response = await client.search("", {
+        searchDepth: "fast",
+        maxResults: 5
+    });
+    return JSON.stringify(response);
+}
+
+export const webSearchSchema: tool = {
+    type: "function",
+    function: {
+        name: "webSearch",
+        description: "Returns results to a web search in the form of a list of items and their links along with a part of their content.",
+        parameters: {
+            type: "object",
+            properties: {
+                query: {
+                    type: "string",
+                    description: "Query to be made."
+                }
+            },
+            required: ["query"]
+        }
+    }
+}
