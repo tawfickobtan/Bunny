abaa980c245e0fafb3144d65e1757e0eca1fd751
Tawfic Kobtan <tawfikcraft@gmail.com>
2026-09-22 10:43:14 +0300

    feat: list files tool

---
+++ src/agent/tools/listFiles.ts
@@ -0,0 +1,30 @@
+import * as fs from 'node:fs/promises';
+import type {tool} from '../types/tool.js';
+import { MockPropertyContext } from 'node:test';
+
+export async function listFiles(directoryPath: string): Promise<string>{
+    try {
+        const files = await fs.readdir(directoryPath);
+        return files.join('\n');
+    }
+    catch(e){
+        return 'Error reading directory: ' + e;
+    }
+}
+
+export const listFilesSchema: tool = {
+    type: 'function',
+    function: {
+        name: 'listFiles',
+        description: 'List files for a given directory.',
+        parameters: {
+            type: 'object',
+            properties: {
+                directoryPath: {
+                    type: 'string',
+                    description: 'Path to list existing files in.'
+                }
+            }
+        }
+    }
+}
