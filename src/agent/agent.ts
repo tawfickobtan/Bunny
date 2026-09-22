0d541f9e20d99504cbba2f15420ca25cd8a5e54c
Tawfic Kobtan <tawfikcraft@gmail.com>
2026-09-22 11:01:26 +0300

    feat: list directory files

--- src/agent/agent.ts
+++ src/agent/agent.ts
@@ -1,26 +1,61 @@
 import client from './types/llm.js';
-import type {tool} from './types/tools.js';
+import type {tool, tool_call} from './types/tool.js';
 import type session from './types/session.js';
+import type {message} from './types/message.js';
+import {readFileSchema, readFile} from './tools/readFile.js';
+import { listFiles, listFilesSchema } from './tools/listFiles.js';
 
 class agent {
     llm: client;
     session: session;
-    tools: tool[]
+    tools: tool[] = [readFileSchema, listFilesSchema];
+    registry: Record<string, any> = {
+        "readFile": readFile,
+        "listFiles": listFiles
+    }
 
-    constructor(client: client, tools: tool[], session: session){
+    constructor(client: client, session: session){
         this.llm = client;
-        this.tools = tools;
         this.session = session;
     }
 
-    addUserMessage(message: string){
+    addUserMessage(message: string): void{
         this.session.messages.push({role: 'user', content: message});
     }
 
     async turn(){
-        const choices = await this.llm.complete(this.session.messages,  this.tools);
-        return choices
+        const msg: message | undefined = await this.llm.complete(this.session.messages,  this.tools);
+        if (msg !== undefined) this.session.messages.push(msg);
+        return msg;
     }
+
+    async executeToolCall(toolCall: tool_call | undefined): Promise<string | undefined>{
+        if (toolCall === undefined) return undefined;
+        if (toolCall.function.name in this.registry){
+            return this.registry[toolCall.function.name](JSON.parse(toolCall.function.arguments));
+        }
+    }
+
+    async run(){
+        while (true){
+            console.log("turned");
+            console.log("---------");
+            const output: message | undefined = await this.turn();
+            const tools: tool_call[] | undefined = output?.tool_calls;
+            if (tools === undefined){
+                return output;
+            }
+
+            for (const toolCall of tools){
+                const toolResponse = await this.executeToolCall(toolCall);
+                if (toolResponse === undefined) continue;
+                this.session.messages.push({role: "tool", tool_call_id: toolCall.id, content: toolResponse});
+                console.log(toolResponse);
+            }
+        }
+    }
+
+
 }
 
 export default agent;
