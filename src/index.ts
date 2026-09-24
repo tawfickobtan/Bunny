838c5cb1fd67f1382511033fd981ad2d5a9dcd40
Tawfic Kobtan <tawfikcraft@gmail.com>
2026-09-24 12:36:34 +0300

    feat: attempting to add express

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import agent from './agent/agent.js';
 import client from './agent/types/llm.js';
 import session from './agent/types/session.js';
 import type { message } from './agent/types/message.js';
+import express  from 'express';
 
+const app = express();
 const llm = new client("deepseek/deepseek-v4.1-flash","deepinfra/fp8");
 const session1:session = new session("1", "Hello", "./", "You're a helpful assistant.");
 
