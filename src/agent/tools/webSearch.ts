136c673be2c1fc1cfc5c324244f8e7652e0aee74
Tawfic Kobtan <tawfikcraft@gmail.com>
2026-09-24 11:46:05 +0300

    feat: Added max results parameter for web search

--- src/agent/tools/webSearch.ts
+++ src/agent/tools/webSearch.ts
@@ -3,12 +3,14 @@ import type {tool} from '../types/tool.js';
 
 const client = tavily({ apiKey: process.env.TAVILY_API_KEY as string});
 
-export async function webSearch({query}: {query: string}): Promise<string>{
+export async function webSearch({query, k}: {query: string; k?: number}): Promise<string>{
+    if (k === undefined) k = 5;
     const response = await client.search(query, {
         searchDepth: "fast",
-        maxResults: 5
+        maxResults: k,
+        include_content: false
     });
-    return JSON.stringify(response);
+    return JSON.stringify(response, null, 2);
 }
 
 export const webSearchSchema: tool = {
@@ -22,9 +24,14 @@ export const webSearchSchema: tool = {
                 query: {
                     type: "string",
                     description: "Query to be made."
+                },
+                k: {
+                    type: "integer",
+                    description: "Max number of result (Default: 5)"
                 }
             },
             required: ["query"]
         }
     }
 }
+
